25c71d849752e4d9a92596837e2a60c18a61fbbc
dksaddy <saddypro1@gmail.com>
2026-08-26 12:16:30 +0600

    Remove unnecessary log.

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -5,8 +5,8 @@ export const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log(
-  "SERVICE KEY EXISTS:",
-  !!process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+// console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+// console.log(
+//   "SERVICE KEY EXISTS:",
+//   !!process.env.SUPABASE_SERVICE_ROLE_KEY
+// );
